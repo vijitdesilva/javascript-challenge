42ed527c3fa1e72874d13b8372a5a619b83b50a3
vijitdesilva <vijit.desilva@gmail.com>
2021-05-01 10:18:54 -0400

    final not checked

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -29,23 +29,23 @@ button.on("click", () => {
 	d3.event.preventDefault();
 	let  inputDate = inputField1.property("value").trim();
 	let  inputCity = inputField2.property("value").toLowerCase().trim();
-	// let  inputState = inputFiled3.property("value").trim();
+	let  inputState = inputField3.property("value").trim();
 	let  inputCountry = inputField4.property("value").trim();
-	// let  inputShape = inputField5.property("value").trim();
+	let  inputShape = inputField5.property("value").trim();
 // Filter by field matching input value
 	let  filterDate = data.filter(data => data.datetime === inputDate);
 	console.log(filterDate)
 	let  filterCity = data.filter(data => data.city === inputCity);
 	console.log(filterCity)
-	// let  filterState = data.filter(data => data.state === inputState);
-	// console.log(filterState)
+	let  filterState = data.filter(data => data.state === inputState);
+	console.log(filterState)
 	let  filterCountry = data.filter(data => data.country === inputCountry);
 	console.log(filterCountry)
-	// let  filterShape = data.filter(data => data.shape === inputShape);
-	// console.log(filterShape)
-	//let  filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity && data.state === inputState && data.country === inputCountry && data.shape === inputShape);
-	//console.log(filterData)
-	let  filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity  && data.country === inputCountry);
+	let  filterShape = data.filter(data => data.shape === inputShape);
+	console.log(filterShape)
+	// let  filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity && data.state === inputState && data.country === inputCountry && data.shape === inputShape);
+	// console.log(filterData)
+	let  filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity  && data.country === inputCountry && data.state === inputState);
 	console.log(filterData)
 	
 
@@ -53,17 +53,17 @@ button.on("click", () => {
 	tbody.html("");
 
 	let response = {
-		filterData, filterCity, filterDate, filterCountry
-		//filterData, filterCity, filterDate, filterState, filterCountry, filterShape
+		// filterData, filterCity, filterDate, filterCountry, filterState 
+		filterData, filterCity, filterDate, filterState, filterCountry, filterShape
 	}
 
 	if (response.filterData.length !== 0) {
 		populate(filterData);
 	}
-		else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0 || response.filterCountry.length !== 0))){
-			populate(filterCity) || populate(filterDate) || populate(filterCountry);
-		//else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0 || response.filterState.length !== 0 || response.filterCountry.length !== 0 || response.filterShape.length !== 0))){
-			//populate(filterCity) || populate(filterDate) || populate(filterState) || populate(filterCountry) || populate(filterShape);
+		//else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0 || response.filterCountry.length !== 0 ))){
+			//populate(filterCity) || populate(filterDate) || populate(filterCountry);
+		else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0 || response.filterState.length !== 0 || response.filterCountry.length !== 0 || response.filterShape.length !== 0))){
+			populate(filterCity) || populate(filterDate) || populate(filterState) || populate(filterCountry) || populate(filterShape);
 		}
 		else {
 			tbody.append("tr").append("td").text("No results found!"); 
