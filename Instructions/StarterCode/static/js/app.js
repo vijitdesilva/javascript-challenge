0787b41fc166552a8a753ef82ee170307a19644d
vijitdesilva <vijit.desilva@gmail.com>
2021-04-30 21:36:11 -0400

    updating

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -6,7 +6,9 @@ let tbody = d3.select("tbody");
 let button = d3.select("#filter-btn");
 let inputField1 = d3.select("#datetime");
 let inputField2 = d3.select("#city");
-let inputField3 = d3.select("#country");
+let inputField3 = d3.select("#state");
+let inputField4 = d3.select("#country");
+let inputField5 = d3.select("#shape");
 let resetbtn = d3.select("#reset-btn");
 let columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
@@ -27,33 +29,34 @@ button.on("click", () => {
 	d3.event.preventDefault();
 	let  inputDate = inputField1.property("value").trim();
 	let  inputCity = inputField2.property("value").toLowerCase().trim();
-	let  inputCountry = inputField3.property("value").trim();
+	let  inputState = inputFiled3.property("value").trim();
+	let  inputCountry = inputField4.property("value").trim();
 // Filter by field matching input value
 	let  filterDate = data.filter(data => data.datetime === inputDate);
 	console.log(filterDate)
 	let  filterCity = data.filter(data => data.city === inputCity);
 	console.log(filterCity)
-	let  filterCountry= data.filter(data => data.country === inputCountry);
+	let  filterState = date.filter(data => data.state === inputState);
+	console.log(filterState)
+	let  filterCountry = data.filter(data => data.country === inputCountry);
 	console.log(filterCountry)
-	let  filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity && data.country === inputCountry);
+	let  filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity && data.state === inputState && data.country === inputCountry);
 	console.log(filterData)
-	
-	// let  filterData = data.filter(data => data.datetime === inputDate && data.city === inputCity && === inputCountry);
-	// console.log(filterData)
+
 	
 
 	// Add filtered sighting to table
 	tbody.html("");
 
 	let response = {
-		filterData, filterCity, filterDate, filterCountry
+		filterData, filterCity, filterDate, filterState, filterCountry
 	}
 
 	if (response.filterData.length !== 0) {
 		populate(filterData);
 	}
-		else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0 || response.filterCountry.length !== 0))){
-			populate(filterCity) || populate(filterDate) || populate(filterCountry);
+		else if (response.filterData.length === 0 && ((response.filterCity.length !== 0 || response.filterDate.length !== 0 || response.filterState.length !== 0 || response.filterCountry.length !== 0))){
+			populate(filterCity) || populate(filterDate) || populate(filterState) || populate(filterCountry);
 	
 		}
 		else {
